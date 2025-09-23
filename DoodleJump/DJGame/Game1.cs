264fc7cf9a07619cf6c7a5a825ec0c58815f81b0
Etienne Caulier <152507490+ETIENNE-CLR@users.noreply.github.com>
2025-09-23 09:07:14 +0200

    ajout des classes pour gérer les fenètres du jeu

--- DoodleJump/DJGame/Game1.cs
+++ DoodleJump/DJGame/Game1.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.Xna.Framework;
+﻿using System.Collections.Generic;
+using DJGame.Controllers;
+using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 
@@ -6,9 +8,15 @@ namespace DJGame
 {
     public class Game1 : Game
     {
+        // Champs de la classe...
+        private static Rectangle screenDimensions;
         private GraphicsDeviceManager _graphics;
         private SpriteBatch _spriteBatch;
 
+        // Propriétés de la classe...
+        public static Rectangle ScreenDimensions { get => screenDimensions; }
+
+        // Constructeur de la classe...
         public Game1()
         {
             _graphics = new GraphicsDeviceManager(this);
@@ -16,9 +24,20 @@ namespace DJGame
             IsMouseVisible = true;
         }
 
+        // Méthodes de la classe...
+        private void ChangeScreenDimensions(int baseWidth = 640)
+        {
+            _graphics.PreferredBackBufferWidth = baseWidth;
+            _graphics.PreferredBackBufferHeight = ((baseWidth * 3) / 2);
+            Game1.screenDimensions = new Rectangle(0, 0, _graphics.PreferredBackBufferWidth, _graphics.PreferredBackBufferHeight);
+            _graphics.ApplyChanges();
+        }
+
         protected override void Initialize()
         {
             // TODO: Add your initialization logic here
+            this.Window.Title = "Doodle Jump";
+            ChangeScreenDimensions(520);
 
             base.Initialize();
         }
@@ -28,6 +47,8 @@ namespace DJGame
             _spriteBatch = new SpriteBatch(GraphicsDevice);
 
             // TODO: use this.Content to load your game content here
+            SceneManager.activeScene = null;
+            SceneManager.activeScene.LoadContent(Content);
         }
 
         protected override void Update(GameTime gameTime)
@@ -36,6 +57,7 @@ namespace DJGame
                 Exit();
 
             // TODO: Add your update logic here
+            SceneManager.activeScene.Update(gameTime);
 
             base.Update(gameTime);
         }
@@ -45,6 +67,9 @@ namespace DJGame
             GraphicsDevice.Clear(Color.CornflowerBlue);
 
             // TODO: Add your drawing code here
+            _spriteBatch.Begin();
+            SceneManager.activeScene.Draw(_spriteBatch, gameTime);
+            _spriteBatch.End();
 
             base.Draw(gameTime);
         }
---
+++ DoodleJump/DJGame/Models/Windows/MonogameWindow.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DJGame.Interfaces;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace DJGame.Models.Windows
+{
+    public class MonogameWindow : IMonogameElement
+    {
+        public void Draw(SpriteBatch spriteBatch, GameTime gameTime)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void LoadContent(ContentManager content)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(GameTime gameTime)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
